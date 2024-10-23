fa89226ff70613da54c2011d68fe1a8dd9db7509
Fernando Fernandez <frfernandezdev@gmail.com>
2024-10-23 11:16:16 -0400

    feat: remove env

--- src/apps/api/api.module.ts
+++ src/apps/api/api.module.ts
@@ -17,11 +17,6 @@ import { ApiHederaModule } from "./hedera/hedera.module";
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
         DATABASE_URL: Joi.string().required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.number().required(),
-        DB_DATABASE: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRES_IN: Joi.string().required(),
         HEDERA_OPERATOR_ID: Joi.string().required(),
